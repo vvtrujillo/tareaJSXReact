93813dc5d5a0f1ae11f122d83cb3f392b8a87cfb
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-07-25 23:11:46 -0400

    commit para tarea de bootcamp

---
+++ src/Componentes/Primero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Primero extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <ul>
+                    <li>Learn react</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a Marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Primero;
